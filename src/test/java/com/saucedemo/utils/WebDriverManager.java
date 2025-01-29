dcc58e7827a6f4eeb173d303efa6ee7343a3764d
Dhanunjaya Galipalli <dhanu.galipalli@gmail.com>
2025-01-29 18:38:51 +0000

    head-less run

--- src/test/java/com/saucedemo/utils/WebDriverManager.java
+++ src/test/java/com/saucedemo/utils/WebDriverManager.java
@@ -2,13 +2,24 @@ package com.saucedemo.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebDriverManager {
     private static WebDriver driver;
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            
+            // Add headless options if running in CI environment
+            String chromeOptions = System.getenv("CHROME_OPTIONS");
+            if (chromeOptions != null && !chromeOptions.isEmpty()) {
+                for (String option : chromeOptions.split("\\s+")) {
+                    options.addArguments(option);
+                }
+            }
+            
+            driver = new ChromeDriver(options);
             driver.manage().window().maximize();
         }
         return driver;
